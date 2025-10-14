b982e1da921bbc645e1ed66a6ec4d84e06a7caca
tommaso <tvenza@sienaimaging.it>
2025-10-14 17:21:46 +0200

    display students from database in tailwind view - used layout full page component as explained in livewire docs

---
+++ app/Livewire/ListStudent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Student;
+use Livewire\Component;
+use Livewire\Attributes\Layout;
+
+class ListStudent extends Component
+{
+    // full component layout as explained in livewire 3 documentation
+    #[Layout('layouts.app')]
+
+    // render method
+    public function render()
+    {
+        return view('livewire.list-student', [
+            // passing data to view
+            'students' => Student::paginate()
+        ]);
+    }
+}
---
+++ resources/views/livewire/list-student.blade.php
@@ -0,0 +1,51 @@
+<div class="flex items-center justify-center min-h-screen bg-gray-100">
+  <div>
+    <div class="overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-gray-200 w-full flex items-center justify-center px-4">
+      <table class="min-w-full text-sm text-gray-800">
+        <thead class="bg-slate-800 text-black">
+          <tr>
+            <th scope="col" class="py-3.5 pl-6 pr-3 text-left font-semibold">ID</th>
+            <th scope="col" class="px-3 py-3.5 text-left font-semibold">Name</th>
+            <th scope="col" class="px-3 py-3.5 text-left font-semibold">Email</th>
+            <th scope="col" class="px-3 py-3.5 text-left font-semibold">Class</th>
+            <th scope="col" class="px-3 py-3.5 text-left font-semibold">Section</th>
+            <th scope="col" class="px-3 py-3.5 text-right font-semibold">Created At</th>
+            <th scope="col" class="py-3.5 pl-3 pr-6 text-right font-semibold">Actions</th>
+          </tr>
+        </thead>
+
+        <tbody class="divide-y divide-gray-200 bg-white ">
+          @foreach ($students as $student)
+            <tr class="hover:bg-gray-50">
+                <td class="whitespace-nowrap py-4 pl-6 pr-3 text-sm font-medium text-gray-900">
+                {{ $student->id }}
+                </td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm font-medium text-gray-900">
+                {{ $student->name }}
+                </td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-700">
+                {{ $student->email }}
+                </td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-700">
+                {{ $student->class->name }}
+                </td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-700">
+                {{ $student->section->name }}
+                </td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-right text-gray-700">
+                2 days ago
+                </td>
+                <td class="relative whitespace-nowrap py-4 pl-3 pr-6 text-right text-sm font-medium">
+                <a href="#" class="text-indigo-600 hover:text-indigo-800">Edit</a>
+                <button class="ml-3 text-rose-600 hover:text-rose-700">Delete</button>
+                </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    
+    <div class="mt-4">{{ $students->links() }}</div>
+  </div>
+</div>
+
