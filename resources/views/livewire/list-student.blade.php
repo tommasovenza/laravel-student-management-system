96ceb32bff3080e3c6ef145323944567c8e91386
tommaso <tvenza@sienaimaging.it>
2025-10-15 15:25:18 +0200

    added route

--- resources/views/livewire/list-student.blade.php
+++ resources/views/livewire/list-student.blade.php
@@ -12,7 +12,7 @@
                 </div>
   
                 <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                    <a href="#"
+                    <a href="{{ route('student.create') }}"
                         class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                         Add Student
                     </a>
@@ -74,7 +74,6 @@
                                         </td>
                                         <td
                                             class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-            
                                             {{ $student->name }}
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
