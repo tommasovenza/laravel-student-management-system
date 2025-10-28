de7d806af14957757707ab526595c8c6895c9a63
tommasovenza <tommasovenza@gmail.com>
2025-10-28 18:22:33 +0100

    create filter functionality using query builder

--- app/Livewire/ListStudent.php
+++ app/Livewire/ListStudent.php
@@ -4,15 +4,27 @@ namespace App\Livewire;
 
 use App\Models\Student;
 use Livewire\Component;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class ListStudent extends Component
 {
+    // search functionality to implement
+    public string $search = '';
+
     // render method
     public function render()
     {
+        $students = Student::query();
+
+        $students = $students->where(function (Builder $query) {
+            $query->where('name', 'like', '%' . $this->search . '%')
+                ->orWhere('email', 'like', '%' . $this->search . '%');
+        })
+            ->paginate();
+
         return view('livewire.list-student', [
             // passing data to view
-            'students' => Student::paginate()
+            'students' => $students
         ]);
     }
 
--- resources/views/livewire/list-student.blade.php
+++ resources/views/livewire/list-student.blade.php
@@ -25,9 +25,15 @@
                         class="absolute pl-2 left-0 top-0 bottom-0 flex items-center pointer-events-none text-gray-500">
                         <x-magnifying-glass />
                     </div>
-  
-                    <input type="text" placeholder="Search students data..." id="search"
-                        class="block rounded-lg border-0 py-2 pl-10 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    
+                    {{-- set wire model here --}}
+                    <input 
+                        wire:model.live="search" 
+                        type="text" 
+                        placeholder="Search students data..." 
+                        id="search"
+                        class="block rounded-lg border-0 py-2 pl-10 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                    />
                 </div>
             </div>
   
