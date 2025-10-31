29238ed4d6422993786828f3cc65fa0dcc9b1040
tommasovenza <tommasovenza@gmail.com>
2025-10-31 11:42:56 +0100

    refactor code using applySearch and Query Builder and reflecting on ListComponent State

--- app/Livewire/ListStudent.php
+++ app/Livewire/ListStudent.php
@@ -5,7 +5,6 @@ namespace App\Livewire;
 use App\Models\Student;
 use Livewire\Component;
 use Livewire\WithPagination;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class ListStudent extends Component
@@ -16,44 +15,25 @@ class ListStudent extends Component
     // search
     public string $search = '';
 
-    public $column = 'id';
-
-    public $direction = 'desc';
-
-    // with this function I was changing only the state of the properties
-    // with click handled by livewire on the blade list-student file
-    public function sort($columnToSort)
+    // this function using $search change state to make a query and 
+    // filter on fronted on user keyboard on input's search
+    public function applySearch(Builder $query): Builder
     {
-        if ($this->column === $columnToSort) {
-            // set this direction
-            $this->direction = $this->direction === 'desc' ? 'asc' : 'desc';
-        } else {
-            // change column
-            $this->column = $columnToSort;
-            $this->direction = $this->direction === 'desc' ? 'asc' : 'desc';
-        }
-        // log
-        logger("column passed: {$columnToSort}");
-        logger("column var: {$this->column}");
-        logger("direction: {$this->direction}");
-        // 
-        $this->resetPage();
+        // make query
+        return $query = $query->where(function (Builder $query) {
+            $query->where('name', 'like', '%' . $this->search . '%')
+                ->orWhere('email', 'like', '%' . $this->search . '%');
+        });
     }
 
     // render method
     public function render()
     {
-        // create a query builder instance
-        $students = Student::query();
+        // instantiate a query object to make complicated queries later on...
+        $queryStudentsObj = Student::query();
 
-        // using it to filter if wire:model search is changing
-        $students = $students->where(function (Builder $query) {
-            $query->where('name', 'like', '%' . $this->search . '%')
-                ->orWhere('email', 'like', '%' . $this->search . '%');
-        });
-
-        // this is just a laravel function to make a query and sort
-        $students->orderBy($this->column, $this->direction);
+        // these are filtered students
+        $students = $this->applySearch($queryStudentsObj);
 
         // returning view
         return view('livewire.list-student', [
@@ -62,12 +42,14 @@ class ListStudent extends Component
         ]);
     }
 
+    // delete student resource
     public function deleteStudent($id)
     {
+        // get student from id
         $student = Student::find($id);
-
+        // delete student resource
         $student->delete();
-
+        // redirect
         return redirect()->route('students.index');
     }
 }
